bc066e4b6b49d46853f08911e6d682fd7818067e
Mike Storey <mike@ssgpp.com>
2023-10-14 12:48:43 -0400

    imporoved mongosh startup

--- docker-quick-test.sh
+++
@@ -1,5 +0,0 @@
-curl http://localhost:8081/api/config/
-curl http://localhost:8081/api/person/
-curl http://localhost:8081/api/person/651dfe6c13605cd1946273c2
-curl -X POST http://localhost:8081/api/person/ -d '{"name":"Foo"}'
-curl -X PATCH http://localhost:8081/api/person/651dfe6c13605cd1946273c2 -d '{"description":"Karmen, Grand PooBa Supreme!"}'
