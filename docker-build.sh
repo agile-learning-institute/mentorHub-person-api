68f7b13fcd7754e12fb264d9b8e2013ca13f28b8
Mike Storey <mike@ssgpp.com>
2023-10-16 19:48:55 -0400

    tagging for GitHub Container Registry

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,6 @@ GOOS=linux GOARCH=amd64 go build -o "institute-person-api" main.go
 export BRANCH=$(git branch --show-current)
 export PATCH=$(git rev-parse $BRANCH)
 echo $BRANCH.$PATCH > PATCH_LEVEL
-docker build . --tag institute-person-api
+docker build . --tag ghcr.io/agile-learning-institute/institute-person-api:latest
+# docker tag institute-person-api:latest ghcr.io/agile-learning-institute/institute-person-api:latest
+# docker push ghcr.io/agile-learning-institute/institute-person-api:latest
