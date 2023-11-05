ac6a41eb71dd3f769073a9b299df6cbc61ca36c3
Mike Storey <mike@ssgpp.com>
2023-11-05 17:17:45 -0500

    multi-arch-docker

--- src/docker/docker-build.sh
+++ src/docker/docker-build.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
 # Build Docker Image
-docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/institute-person-api:latest .
+docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/institute-person-api:latest . $1
 if [ $? -ne 0 ]; then
     echo "Docker build failed"
     exit 1
 fi
 
-# Run the Database and API containers
-if [ "$1" = '--run' ]; then
-    curl https://raw.githubusercontent.com/agile-learning-institute/institute/main/docker-compose/run-local-person-api.sh | /bin/bash
-fi
-
-# Push Docker image (To be removed when CI works)
-if [ "$1" = '--push' ]; then
-    docker push ghcr.io/agile-learning-institute/institute-person-api:latest
-    if [ $? -ne 0 ]; then
-    echo "Docker build failed"
-    exit 1
-        echo "Docker push failed"
-        exit 1
-    fi
-    echo "image pushed"
-fi
+curl https://raw.githubusercontent.com/agile-learning-institute/institute/main/docker-compose/run-local-person-api.sh | /bin/bash
