1132e944bfb5a10ce3a3ee5814ceac5c45bdec9a
Mike Storey <mike@ssgpp.com>
2024-02-18 12:49:50 -0500

    minor docker build improvements

--- src/docker/docker-build.sh
+++ src/docker/docker-build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Ensure we are running in the proper folder"
+if !([[ -d "./src/docker" ]] && [[ -f "./src/main.go" ]]); then 
+    echo "This script must be run from the repository root folder"
+    exit 1
+fi
+
 # Build Docker Image
 docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/mentorhub-person-api:latest .
 if [ $? -ne 0 ]; then
@@ -7,4 +13,5 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-/bin/bash <(curl -o - https://raw.githubusercontent.com/agile-learning-institute/mentorhub/main/docker-configurations/run-local.sh) person-api
+# Start the API container with backing database
+mh up person-api
