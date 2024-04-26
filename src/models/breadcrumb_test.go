fe37ad44ac05ea9436699128b25d4f4d60afcbec
Mike Storey <mike@agile-learning.institute>
2024-04-26 13:26:44 -0400

    breadcrumb testing complete

--- src/models/breadcrumb_test.go
+++ src/models/breadcrumb_test.go
@@ -2,17 +2,22 @@ package models
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func TestNewBreadcrumb(t *testing.T) {
 	crumb, err := NewBreadCrumb("255.255.255.255", "000000000000000000000000", "CORRID")
+	assert.Nil(t, err)
 
-	assert.NotNil(t, err)
+	oid, err := primitive.ObjectIDFromHex("000000000000000000000000")
+	assert.Nil(t, err)
+
 	assert.NotNil(t, crumb)
 	assert.Equal(t, crumb.FromIp, "255.255.255.255")
-	assert.Equal(t, crumb.ByUser, "USERID")
+	assert.Equal(t, crumb.ByUser, oid)
 	assert.Equal(t, crumb.CorrelationId, "CORRID")
-	// TODO: assert time within a few seconds of now
+	assert.WithinDuration(t, time.Now(), crumb.AtTime.Time(), time.Second)
 }
